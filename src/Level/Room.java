85fc6d808ce55128d19d04b324fb02298b31ee5c
Binta1906 <98154259+Binta1906@users.noreply.github.com>
2023-05-16 14:41:50 +0200

    crée une room dans le gamepanel

--- src/Level/Room.java
+++ src/Level/Room.java
@@ -1,11 +1,16 @@
 package Level;
 
+import entity.Player;
+import tile.TileManager;
+
 public class Room {
 	private int idRoom;
 	private Porte porteHaut;
 	private Porte porteBas;
 	private Porte porteGauche;
 	private Porte porteDroite;
+    private Player player;
+	private TileManager tileManager;
 
 	public Room(int idRoom, Porte porteHaut, Porte porteBas, Porte porteGauche, Porte porteDroite) {
 		this.idRoom = idRoom;
@@ -15,6 +20,12 @@ public class Room {
 		this.porteDroite = porteDroite;
 	}
 	
-	
+	public Room(int idRoom) {
+		this.idRoom = idRoom;
+		this.porteHaut = null;
+		this.porteBas = null;
+		this.porteGauche = null;
+		this.porteDroite = null;
+	}
 
 }
